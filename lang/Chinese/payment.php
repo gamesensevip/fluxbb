288568e26a6d47463d19777d3068285cad4be838
Motify <93656397+mtfy@users.noreply.github.com>
2022-03-09 14:26:29 +0000

    Fixed syntax

--- lang/Chinese/payment.php
+++ lang/Chinese/payment.php
@@ -2,24 +2,24 @@
 
 // Language definitions used in payment.php, newpayment.php, payment_query.php and motify_payments.php
 $lang_payment = array(
-    'PayPal 1' => '使用你的網絡瀏覽器登錄你的PayPal帳號。點擊頁面上的%sSend &amp; Request%s。輸入下面的電子郵件地址到發送地址。'。
-    'PayPal 2' => '輸入%s作為金額，歐元作為貨幣，以及下面的備註作為說明，然後點擊%sContinue%s。
-    'PayPal 3' => '現在，根據步驟，選擇上述選項%sSending to a friend%s。
-    'PayPal 4' => '你必須使用%sFriends &amp; Family%s選項來購買 ! 除了&apos;Friends &amp; Family&apos;以外的所有其他付款方式將導致你的Gamsense帳號被永久禁止使用！'。
-    'PayPal 5' => '完成付款。請確保提交交易ID，以及與您的PayPal賬戶相關的電子郵件地址。管理員將手動審查您的付款。這個過程可能需要24小時，所以請耐心等待！'。
-    'Plan' => '%s的訂閱%s天'。
+    'PayPal 1' => '使用你的網絡瀏覽器登錄你的PayPal帳號。點擊頁面上的%sSend &amp; Request%s。輸入下面的電子郵件地址到發送地址。',
+    'PayPal 2' => '輸入%s作為金額，歐元作為貨幣，以及下面的備註作為說明，然後點擊%sContinue%s。',
+    'PayPal 3' => '現在，根據步驟，選擇上述選項%sSending to a friend%s。',
+    'PayPal 4' => '你必須使用%sFriends &amp; Family%s選項來購買 ! 除了&apos;Friends &amp; Family&apos;以外的所有其他付款方式將導致你的Gamsense帳號被永久禁止使用！'
+    'PayPal 5' => '完成付款。請確保提交交易ID，以及與您的PayPal賬戶相關的電子郵件地址。管理員將手動審查您的付款。這個過程可能需要24小時，所以請耐心等待！',
+    'Plan' => '%s的訂閱%s天',
     'Transaction ID'=> '交易ID',
     'Billing Email' => '您的PayPal電子郵件',
-    'Billing Email Tooltip' => '輸入與你的PayPal帳號相關的電子郵件，以便交易順利以及日後爭議。
-    'Submit' => '提交'。
-    'Cancel' => '取消'。
+    'Billing Email Tooltip' => '輸入與你的PayPal帳號相關的電子郵件，以便交易順利以及日後爭議。',
+    'Submit' => '提交',
+    'Cancel' => '取消',
     '501' => '抱歉!但這個支付選項暫時不可用。', // 支付方式未啟用。
-    '403' => '禁止'。
-    '400' => '請求參數錯誤'。
-    '429' => '你發送了一個現有的交易ID。請最多等待24小時'。
-    'Invoice confirmation' => '發票確認請求成功！'。
+    '403' => '禁止。',
+    '400' => '請求參數錯誤。',
+    '429' => '你發送了一個現有的交易ID。請最多等待24小時。',
+    'Invoice confirmation' => '發票確認請求成功。',
     'Success' => '訂單成功！', // 用於狀態標題
-    'Success Text' => '我們已經收到您的訂單。請耐心等待，讓管理員審核並確認您的付款，最多花費24小時！'。
-    'Failed' => '付款失敗！'。
-    'Awaiting Approval' => '已經發送請求。可能需要24小時的時間讓管理員審查並確認你的付款'。
+    'Success Text' => '我們已經收到您的訂單。請耐心等待，讓管理員審核並確認您的付款，最多花費24小時！',
+    'Failed' => '付款失敗！',
+    'Awaiting Approval' => '已經發送請求。可能需要24小時的時間讓管理員審查並確認你的付款。'
 ); 
