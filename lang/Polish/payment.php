90bc3a72e3fec7ff3a544c5dc02be652000e46f1
Jakub Kun <80093182+nvx0@users.noreply.github.com>
2022-06-10 16:26:01 +0200

    Fixed translations.
    Some words have been changed to more correct ones. Fixed small typos.

--- lang/Polish/payment.php
+++ lang/Polish/payment.php
@@ -5,7 +5,7 @@
 
 $lang_payment = array(
     'PayPal 1'          	=>  'Zaloguj się na PayPal przy użyciu swojej przeglądarki. Kliknij %sSend &amp; Request%s na górze strony. Wpisz email który jest podany poniżej jako odbiorca.',
-    'PayPal 2'              =>  'Wpisz %s jako kwote, EUR jako walute, i poniższą fraze jako opis. %sContinue%s.',
+    'PayPal 2'              =>  'Wpisz %s jako kwotę, EUR jako walutę, i poniższy opis. %sContinue%s.',
     'PayPal 3'              =>  'Teraz wybierz powyższą opcję %sSending to a friend%s, kiedy zostaniesz o to poproszony.',
     'PayPal 4'              =>  'Musisz wysłać pieniądze za pomocą funkcji %sFriends &amp; Family%s! Płatności za które nie zapłacisz metodą &apos;Friends &amp; Family&apos; poskutkują blokadą twojego konta!',
     'PayPal 5'              =>  'Dokończ płatność. Upewnij się że opis płatności zawiera ID Transakcji, upewnij się również czy twój adres email jest połączony z twoim kontem PayPal. Administrator ręcznię sprawdzi twoją płatność. Ta procedura może potrwać aż do 24h, więc prosimy uzbroić się w cierpliwość.',
@@ -18,10 +18,10 @@ $lang_payment = array(
     '501'                   =>  'Metoda płatności nie jest obecnie włączona. Prosimy spróbować pozniej.', // Payment method not enabled
     '403'          			=>  'Zabronione',
     '400'                   =>  'Złe parametry zapytania',
-  	'429'          			=>  'Wysłałeś istniejące ID Transakcji, może to potrwać aż do 24h.',
+  	'429'          			=>  'Wysłałeś istniejące ID transakcji, może to potrwać aż do 24h.',
     'Invoice confirmation'  =>  'Zapytanie o potwierdzenie faktury powiodło się!',
     'Success'               =>  'Sukces!', // Used for status title
-    'Success Text'          =>  'Otrzymaliśmy twoje zamówienie. Proszę uzbroić się w cierpliwość ponieważ może to zająć aż 24h żeby Administrator ocenił i zatwierdził twoją płatność!',
-	'Failed' 				=>  'Płatnośc nie powiodła się.'
-    'Awaiting Approval'     =>  'Masz oczekującą fakture(y). Może to zająć do 24h żeby Administrator ocenił i zatwierdził twoją płatność.'
+    'Success Text'          =>  'Otrzymaliśmy twoje zamówienie. Prosimy uzbroić się w cierpliwość ponieważ może to zająć aż 24h żeby Administrator ocenił i zatwierdził twoją płatność!',
+	'Failed' 				=>  'Płatność nie powiodła się.'
+    'Awaiting Approval'     =>  'Masz oczekujące zamówienie. Może to zająć do 24h żeby Administrator ocenił i zatwierdził twoją płatność.'
 );
